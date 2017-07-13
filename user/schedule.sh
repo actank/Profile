be2f5863baa9505a4d31bdc8fa17352d7f844ca5
yujishi <yujishi@higohappy.com>
2017-07-13 15:12:29 +0800

    MOD:update

--- user/schedule.sh
+++ user/schedule.sh
@@ -3,9 +3,10 @@
 
 if [ -n $1 ]
 then
-    if [[ $1 == "short_dump" ]]
+    if [[ $1 == "middle_dump" ]]
     then
         echo $1
+        python dump_user_action.py
     elif [[ $1 == "short_category_preference" ]]
     then
         echo $1
@@ -14,6 +15,6 @@ then
         echo $1
     elif [[ $1 == "help" || $# != 1 ]]
     then
-        echo "usage: sh schedule.sh dump|category_preference|brand_preference"
+        echo "usage: sh schedule.sh middle_dump|category_preference|brand_preference"
     fi
 fi
